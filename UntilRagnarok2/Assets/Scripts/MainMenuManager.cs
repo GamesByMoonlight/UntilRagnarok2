2cec92b233ac0f63a1967c005e603d92e5101dd3
OYIANAKIS\Oyiannakis <oyianakis@gmail.com>
2019-07-25 14:16:16 -0500

    Changed some materials, text elements, created menu screen added Norse font to text mesh pro... added skybox

---
+++ UntilRagnarok2/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
